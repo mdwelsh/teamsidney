a9f1287e0702ad2107309eebb93f2e619879eef4
Matt Welsh <mdw@mdw.la>
2018-09-12 22:29:50 -0700

    Checkin and read mode code

--- arduino/HouseLights/HouseLights.ino
+++ arduino/HouseLights/HouseLights.ino
@@ -7,61 +7,89 @@
 #define USE_SERIAL Serial
 
 WiFiMulti wifiMulti;
+HTTPClient http;
+String* curMode = NULL;
 
 void setup() {
+  curMode = new String("none");
+  pinMode(LED_BUILTIN, OUTPUT);
 
-    USE_SERIAL.begin(115200);
+  USE_SERIAL.begin(115200);
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+  for (uint8_t t = 4; t > 0; t--) {
+    USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
+    USE_SERIAL.flush();
+    delay(1000);
+  }
+  wifiMulti.addAP("theonet", "juneaudog");
+}
 
-    USE_SERIAL.println();
-    USE_SERIAL.println();
-    USE_SERIAL.println();
+void flashLed() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+}
 
-    for(uint8_t t = 4; t > 0; t--) {
-        USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
-        USE_SERIAL.flush();
-        delay(1000);
+void checkin() {
+  USE_SERIAL.print("MDW: MAC address is ");
+  USE_SERIAL.print(WiFi.macAddress());
+  USE_SERIAL.print("\nMDW: IP address is ");
+  USE_SERIAL.print(WiFi.localIP().toString());
+  USE_SERIAL.print("\n");
+
+  String url = "https://team-sidney.firebaseio.com/checkin/" + WiFi.macAddress() + ".json";
+  http.begin(url);
+
+  // {".sv": "timestamp"} writes the server's timestamp value when the payload is received.
+  // Firebase is so cool.
+  String payload = "{\"timestamp\": {\".sv\": \"timestamp\"}, \"mac\": \"" + WiFi.macAddress() + "\", \"ip\": \"" + WiFi.localIP().toString() + "\", \"mode\": \"" + *curMode +"\"}";
+
+  USE_SERIAL.print("[HTTP] PUT " + url + "\n");
+  USE_SERIAL.print(payload + "\n");
+  int httpCode = http.PUT(payload);
+
+  if (httpCode > 0) {
+    USE_SERIAL.printf("[HTTP] Response code: %d\n", httpCode);
+    String payload = http.getString();
+    USE_SERIAL.println(payload);
+  } else {
+    USE_SERIAL.printf("[HTTP] failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+  http.end();
+}
+
+void readMode() {
+  String url = "https://team-sidney.firebaseio.com/strips/" + WiFi.macAddress() + ".json";
+  http.begin(url);
+
+  USE_SERIAL.print("[HTTP] GET " + url + "\n");
+  int httpCode = http.GET();
+
+  if (httpCode > 0) {
+    USE_SERIAL.printf("[HTTP] Response code: %d\n", httpCode);
+    if (curMode != NULL) {
+      delete curMode;
     }
-
-    wifiMulti.addAP("theonet", "juneaudog");
+    curMode = new String(http.getString());
+    USE_SERIAL.println(*curMode);
+  } else {
+    USE_SERIAL.printf("[HTTP] failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+  http.end();
 }
 
 void loop() {
-    // wait for WiFi connection
-    if((wifiMulti.run() == WL_CONNECTED)) {
-
-        USE_SERIAL.print("MDW: MAC address is ");
-        USE_SERIAL.print(WiFi.macAddress());
-        USE_SERIAL.print("\n");
-
-        HTTPClient http;
-
-        USE_SERIAL.print("[HTTP] begin...\n");
-        // configure traged server and url
-        //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
-        //http.begin("http://example.com/index.html"); //HTTP
-        //http.begin("https://www.mdw.la/index.html");
-        http.begin("https://team-sidney.firebaseio.com/strips/strip3.json");
-
-        USE_SERIAL.print("[HTTP] GET...\n");
-        // start connection and send HTTP header
-        int httpCode = http.GET();
-
-        // httpCode will be negative on error
-        if(httpCode > 0) {
-            // HTTP header has been send and Server response header has been handled
-            USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
-
-            // file found at server
-            if(httpCode == HTTP_CODE_OK) {
-                String payload = http.getString();
-                USE_SERIAL.println(payload);
-            }
-        } else {
-            USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        }
-
-        http.end();
-    }
-
-    delay(5000);
+  if ((wifiMulti.run() != WL_CONNECTED)) {
+    delay(10000);
+    return;
+  }
+  flashLed();
+  checkin();
+  readMode();
+  USE_SERIAL.println("Current mode: " + *curMode + "\n");
+  
+  delay(30 * 1000);
 }
