45c0684350d171087b9d4cb0ddaf82ebd40e2bdb
Matt Welsh <mdw@mdw.la>
2018-12-16 20:28:45 -0800

    Working on this

--- arduino/Escher/Escher.ino
+++ arduino/Escher/Escher.ino
@@ -1,12 +1,16 @@
+#include <vector>
+
 #include <Wire.h>
 #include <AccelStepper.h>
 #include <Adafruit_MotorShield.h>
+
+#define MAX_SPEED 50.0
+#define ACCELERATION 500.0
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 Adafruit_StepperMotor *myStepper1 = AFMS.getStepper(200, 1);
 Adafruit_StepperMotor *myStepper2 = AFMS.getStepper(200, 2);
-
 void forwardstep1() {  
   myStepper1->onestep(FORWARD, SINGLE);
 }
@@ -20,29 +24,60 @@ void backwardstep2() {
   myStepper2->onestep(BACKWARD, DOUBLE);
 }
 
+class Escher {
+  public:
+  Escher(AccelStepper& xstepper, AccelStepper& ystepper) : _xstepper(xstepper), _ystepper(ystepper) {
+    _xstepper.setMaxSpeed(MAX_SPEED);
+    _xstepper.setAcceleration(ACCELERATION);
+    _ystepper.setMaxSpeed(MAX_SPEED);
+    _ystepper.setAcceleration(ACCELERATION);
+  }
+
+  void moveTo(int x, int y) {
+    Serial.printf("moveTo (%d, %d)\n", x, y);
+    _xstepper.moveTo(x);
+    _ystepper.moveTo(y);
+  }
+
+  void push(int x, int y) {
+    Serial.printf("push (%d, %d)\n", x, y);
+    std::pair<int, int> coord(x, y);
+    _pending.push_back(coord);
+  }
+
+  void run() {
+    if (_xstepper.distanceToGo() == 0 && _ystepper.distanceToGo() == 0 && !_pending.empty()) {
+      std::pair<int, int> front = _pending.front();
+      Serial.printf("Next waypoint: (%d, %d)\n", front.first, front.second);
+      _xstepper.moveTo(front.first);
+      _ystepper.moveTo(front.second);
+      _pending.erase(_pending.begin());
+    }
+
+    _xstepper.run();
+    _ystepper.run();
+  }
+
+  private:
+  AccelStepper& _xstepper;
+  AccelStepper& _ystepper;
+  std::vector<std::pair<int, int>> _pending;
+  
+};
+
 AccelStepper stepper1(forwardstep1, backwardstep1);
 AccelStepper stepper2(forwardstep2, backwardstep2);
+Escher escher(stepper1, stepper2);
 
-void setup()
-{  
+void setup() {  
   AFMS.begin(); // Start the bottom shield
-   
-  stepper1.setMaxSpeed(100.0);
-  stepper1.setAcceleration(100.0);
-  stepper1.moveTo(24);
-    
-  stepper2.setMaxSpeed(200.0);
-  stepper2.setAcceleration(100.0);
-  stepper2.moveTo(50000);
+
+  escher.push(100, 0);
+  escher.push(100, 100);
+  escher.push(0, 100);
+  escher.push(0, 0);
 }
 
-void loop()
-{
-    // Change direction at the limits
-    if (stepper1.distanceToGo() == 0) stepper1.moveTo(-stepper1.currentPosition());
-
-    if (stepper2.distanceToGo() == 0) stepper2.moveTo(-stepper2.currentPosition());
-
-    stepper1.run();
-    stepper2.run();
+void loop() {
+  escher.run();
 }
