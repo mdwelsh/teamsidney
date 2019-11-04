70541316a2a7a3c5d1128d6111084aca00246cf4
Matt Welsh <mdw@mdw.la>
2019-11-03 22:08:40 -0800

    Adding some files.

---
+++ arduino/Escher/Escher/EscherDeviceConfig.h
@@ -0,0 +1,10 @@
+#ifndef ESCHER_DEVICE_CONFIG_H
+#define ESCHER_DEVICE_CONFIG_H
+
+/* Important - do not commit this file to github!!! */
+
+#define DEVICE_SECRET "reptiles"
+#define WIFI_NETWORK "theonet"
+#define WIFI_PASSWORD "juneaudog"
+
+#endif // ESCHER_DEVICE_CONFIG_H
