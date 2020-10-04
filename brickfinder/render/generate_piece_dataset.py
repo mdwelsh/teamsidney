8d88a12855d3db70706ed1395a0926077bd6903b
Matt Welsh <mdw@mdw.la>
2020-10-04 11:59:20 -0700

    Some improvements to lighting.

--- brickfinder/render/generate_piece_dataset.py
+++ brickfinder/render/generate_piece_dataset.py
@@ -13,39 +13,81 @@ from ldraw.pieces import Piece
 from ldraw.tools import get_model
 from ldraw.writers.povray import POVRayWriter
 
+IMAGE_WIDTH = 256
+IMAGE_HEIGHT = 256
+
 
 PARTS = [
-    Brick1X1,
-    Brick1X2,
-    Brick1X3,
-    Brick1X6,
-    Brick2X2,
+    # Brick1X1,
+    # Brick1X2,
+    # Brick1X3,
+    # Brick1X6,
+    # Brick2X2,
     Brick2X3,
-    Brick2X4,
+    # Brick2X4,
 ]
 
-NUM_COLORS=1
+NUM_COLORS = 1
 COLORS = list(ColoursByCode.values())[0:NUM_COLORS]
 
 
+LIGHTS = [
+    [
+        (-200, -200, -200, 200),
+    ],
+    [
+        (200, -200, -200, 200),
+    ],
+    [
+        (-200, 200, -200, 200),
+    ],
+    [
+        (200, 200, -200, 200),
+    ],
+    [
+        (-200, -200, 200, 200),
+    ],
+    [
+        (200, -200, 200, 200),
+    ],
+    [
+        (-200, 200, 200, 200),
+    ],
+    [
+        (200, 200, 200, 200),
+    ],
+]
+
+
+LIGHT_SOURCE_TEMPLATE = """
+  light_source {{
+    <{x}, {y}, {z}>, 1
+    fade_distance {fade_dist} fade_power 2
+    area_light x*70, y*70, 20, 20 circular orient adaptive 0 jitter
+  }}
+"""
+
+# light_source
+# { <0, 1900, 0>, 1
+#  fade_distance 1600 fade_power 2
+#  area_light x*70, y*70, 20, 20 circular orient adaptive 0 jitter
+# }
+#
+# light_source
+# { <2000, 1700, 0>, <1,.8,.4>
+#  fade_distance 1000 fade_power 2
+#  area_light x*70, y*70, 20, 20 circular orient adaptive 0 jitter
+# }
+
+
 POV_TRAILER = """
-light_source
-{ <0, 1900, 0>, 1
-  fade_distance 1600 fade_power 2
-  area_light x*70, y*70, 20, 20 circular orient adaptive 0 jitter
-}
-
-light_source
-{ <2000, 1700, 0>, <1,.8,.4>
-  fade_distance 1000 fade_power 2
-  area_light x*70, y*70, 20, 20 circular orient adaptive 0 jitter
-}
-
 background { color Black }
 
 camera {
-  location <-200.000000, 200.000000, -200.000000>
+  location <-200.000000, 0.000000, 0.000000>
   look_at <0.000000, 0.000000, 0.000000>
+  up y
+  right x
   angle 40
 }
 """
@@ -58,17 +100,21 @@ def gen_piece(part, color, x=0, y=0, z=0):
     return Piece(color, Vector(x, y, z), rot, part)
 
 
-def gen_pov(ldraw_path, pov_path):
+def gen_pov(ldraw_path, pov_path, lights=[]):
     model, parts = get_model(ldraw_path)
 
     with open(pov_path, "w") as pov_file:
         pov_file.write('#include "colors.inc"\n\n')
         writer = POVRayWriter(parts, pov_file)
         writer.write(model)
+        for (x, y, z, fade_dist) in lights:
+            pov_file.write(
+                LIGHT_SOURCE_TEMPLATE.format(x=x, y=y, z=z, fade_dist=fade_dist) + "\n"
+            )
         pov_file.write(POV_TRAILER + "\n")
 
 
-def run_pov(pov_path, image_path, image_width=1024, image_height=768):
+def run_pov(pov_path, image_path, image_width=IMAGE_WIDTH, image_height=IMAGE_HEIGHT):
     cmd = [
         "povray",
         f"-i{pov_path}",
@@ -81,25 +127,28 @@ def run_pov(pov_path, image_path, image_width=1024, image_height=768):
     result.check_returncode()
 
 
-def gen_image(ldraw_path, image_path):
+def gen_image(ldraw_path, image_path, lights):
     print(f"   {image_path}\r", end="")
     with tempfile.NamedTemporaryFile(suffix=".pov", delete=False) as povfile:
         povfile.close()
-        gen_pov(ldraw_path, povfile.name)
+        gen_pov(ldraw_path, povfile.name, lights)
         run_pov(povfile.name, image_path)
 
 
 def gen_images(outdir, part, color, num_images):
     retval = []
-    for index in range(num_images):
-        piece = gen_piece(part, color)
-        out_image_filename = os.path.join(outdir, "image{:05d}.jpg".format(index))
-        with tempfile.NamedTemporaryFile(
-            mode="w", suffix=".ldr", delete=False
-        ) as ldrfile:
-            ldrfile.write(str(piece) + "\n")
-        gen_image(ldrfile.name, out_image_filename)
-        retval.append(out_image_filename)
+    imagenum = 0
+    for _ in range(num_images):
+        for lights in LIGHTS:
+            piece = gen_piece(part, color)
+            out_image_filename = os.path.join(outdir, "image{:05d}".format(imagenum))
+            with tempfile.NamedTemporaryFile(
+                mode="w", suffix=".ldr", delete=False
+            ) as ldrfile:
+                ldrfile.write(str(piece) + "\n")
+            gen_image(ldrfile.name, out_image_filename, lights)
+            retval.append(out_image_filename)
+            imagenum += 1
     return retval
 
 
@@ -132,9 +181,14 @@ def gen_dataset(outdir: str, images_per_part: int):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--outdir", help="Output directory for generated dataset.")
     parser.add_argument(
-        "--images_per_part", help="Number of output images per class.", type=int
+        "--outdir", help="Output directory for generated dataset.", required=True
+    )
+    parser.add_argument(
+        "--images_per_part",
+        help="Number of output images per class.",
+        type=int,
+        default=10,
     )
     args = parser.parse_args()
     gen_dataset(args.outdir, args.images_per_part)
